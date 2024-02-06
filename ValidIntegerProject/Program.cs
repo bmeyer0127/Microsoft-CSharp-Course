ce5814bb7a329dc844e3cb2dcb2126bfc22a968a
Brett Meyer <32486568+bmeyer0127@users.noreply.github.com>
2024-02-06 16:45:42 -0500

    Add files via upload

---
+++ ValidIntegerProject/Program.cs
@@ -0,0 +1,24 @@
+﻿// Prompt user for valid integer input
+Console.Write("Please input a number between 5 and 10: ");
+
+// Acceptable parameters initialization
+bool validInteger = false;
+int potentialInteger;
+
+// Iteration block that confirms or reiterates the request
+do
+{
+  string? userInput = Console.ReadLine();
+  Console.WriteLine();
+
+  if (int.TryParse(userInput, out potentialInteger) && (potentialInteger > 4) && (potentialInteger < 11))
+  {
+    validInteger = true;
+    continue;
+  }
+
+  Console.WriteLine("Looks like that's not what we're looking for, idiot.\n");
+  Console.Write("Try again?: ");
+} while (validInteger == false);
+
+Console.WriteLine("Amazing. Thank you");
---
+++ ValidStringProject/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Which of the following are you logging in as?\n");
+Console.WriteLine("-\tAdministrator\n-\tManager\n-\tUser\n");
+Console.Write("Please type your answer here: ");
+
+bool validInput = false;
+string[] validInputs = ["administrator", "manager", "user"];
+string? userInput;
+
+// String validation loop
+do
+{
+  userInput = Console.ReadLine();
+  string? polishedInput = userInput.Trim().ToLower();
+
+  foreach (string input in validInputs)
+  {
+    if (polishedInput == input)
+      validInput = true;
+    continue;
+  }
+  if (validInput)
+    continue;
+
+  // Response for failed login attempt
+  Console.WriteLine("Looks like that didn't work for us. What are you logging in as?\n");
+  Console.WriteLine("-\tAdministrator\n-\tManager\n-\tUser\n");
+  Console.Write("Please type your answer here: ");
+} while (!validInput);
+
+// Response for valid login attempt
+Console.WriteLine($"Neat! Welcome in {userInput}!");
